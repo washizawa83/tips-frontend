35f30d3dcc98e7610f0cfbe8e560cb8963f8e963
washizawa haruya (JSL) <129074090+washizawa83@users.noreply.github.com>
2024-06-08 20:53:37 +0900

    Github Actionsに単体テスト追加 (#3)
    * Github Actionsに単体テスト追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,16 @@
 /** @type {import('tailwindcss').Config} */
-const withMT = require("@material-tailwind/react/utils/withMT");
+const withMT = require('@material-tailwind/react/utils/withMT');
 module.exports = withMT({
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
-  theme: {
-    extend: {
-      colors: {
-        'tips-gray': "#41434D",
-        'tips-dark-gray': '#38373D',
-        'tips-black': '#2D2C31',
-        'tips-gray-text': '#F1F0FA'
-      }
+    content: ['./src/**/*.{js,jsx,ts,tsx}'],
+    theme: {
+        extend: {
+            colors: {
+                'tips-gray': '#41434D',
+                'tips-dark-gray': '#38373D',
+                'tips-black': '#2D2C31',
+                'tips-gray-text': '#F1F0FA',
+            },
+        },
     },
-  },
-  plugins: [],
-})
-
+    plugins: [],
+});
