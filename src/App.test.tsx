587bd5c40babe59d5ddb7085adc43d672c40fb34
washizawa83 <washizawa83@gmail.com>
2024-06-08 18:08:56 +0900

    eslintエラー修正・テストコード追加

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,15 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import React from 'react';
-import { act } from 'react';
 import App from './App';
 
 describe('App component', () => {
     test('renders home page', () => {
-        act(() => {
-            render(<App />);
-        });
-
-        // "ホーム" テキストが表示されていることを確認します
+        render(<App />);
         const homeElement = screen.getByText(/ホーム/i);
         expect(homeElement).toBeInTheDocument();
     });
--- src/app/components/layouts/header.test.tsx
+++ src/app/components/layouts/header.test.tsx
@@ -1,10 +1,33 @@
-// import React from "react";
-// import { render, screen } from "@testing-library/react";
-// import { Header } from "./header";
+import { fireEvent, render, screen } from '@testing-library/react';
+import React from 'react';
+import '@testing-library/jest-dom/extend-expect';
+import { BrowserRouter } from 'react-router-dom';
+import { Header } from './header';
 
-// test("renders learn react link", () => {
-//   const mockHandleOpenSidebarEmit = () => {};
-//   render(<Header openSidebarEmit={mockHandleOpenSidebarEmit} />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
+describe('Header component', () => {
+    test('renders anchor links in large screens', () => {
+        render(
+            <BrowserRouter>
+                <Header openSidebarEmit={jest.fn()} />
+            </BrowserRouter>,
+        );
+        const notificationLink = screen.getByText(/通知/i);
+        const settingsLink = screen.getByText(/設定/i);
+        const logoutLink = screen.getByText(/ログアウト/i);
+        expect(notificationLink).toBeInTheDocument();
+        expect(settingsLink).toBeInTheDocument();
+        expect(logoutLink).toBeInTheDocument();
+    });
+
+    test('calls openSidebarEmit on menu button click', () => {
+        const mockOpenSidebarEmit = jest.fn();
+        render(
+            <BrowserRouter>
+                <Header openSidebarEmit={mockOpenSidebarEmit} />
+            </BrowserRouter>,
+        );
+        const menuButton = screen.getByLabelText(/open drawer/i);
+        fireEvent.click(menuButton);
+        expect(mockOpenSidebarEmit).toHaveBeenCalledTimes(1);
+    });
+});
