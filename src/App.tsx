f9b094c5d7ba0bff1db24cae1abc4a935a9fa699
washizawa83 <washizawa83@gmail.com>
2024-06-16 02:49:45 +0900

    認証周り作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,8 @@
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import { Amplify } from 'aws-amplify';
+import '@aws-amplify/ui-react/styles.css';
 import React from 'react';
+import config from './amplifyconfiguration.json';
 import './App.css';
 import { Box } from '@mui/material';
 import { ThemeProvider } from '@mui/material/styles';
@@ -8,7 +12,9 @@ import { HomePageComponent } from './app/components/pages/HomePageComponent';
 import { PostPageComponent } from './app/components/pages/PostPageComponent';
 import { theme } from './app/themes/theme';
 
-function App() {
+Amplify.configure(config);
+
+function App({ signOut, user }: any) {
     return (
         <Box className='min-w-full max-w-full min-h-screen max-h-screen'>
             <ThemeProvider theme={theme}>
@@ -28,4 +34,4 @@ function App() {
     );
 }
 
-export default App;
+export default withAuthenticator(App);
